d6590db380de0044ae926e484ad8a92f74118d90
Vishnuprasanth-2002 <s.vishnuprasanth14@gmail.com>
2023-09-26 23:13:06 +0530

    readme file

--- app/script.js
+++ app/script.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
     const user = [];
+    $("#hide").click(function() {
+        const passwordInput = $("#password");
+    
+        if (passwordInput.attr("type") === "password") {
+          passwordInput.attr("type", "text");
+        } else {
+          passwordInput.attr("type", "password");
+        }
+      });
     const btnsubmit = $("#submit");
     btnsubmit.click(function (e) {
       e.preventDefault();
       const email = $("#email").val();
       const password = $("#password").val();
+      const errorEmail = document.querySelector("#email-error");
+      const error = document.querySelector("#pass-error");
+
       if(email){
+        errorEmail.innerHTML="";
           if(passwordCheck()){
+            error.innerHTML="";
               const profile = {
                   email: email,
                   password: password,
@@ -14,10 +28,10 @@ $(document).ready(function () {
                 user.push(profile);
                 console.log(JSON.stringify(user));
           }else{
-              alert(" password should be atleast 8 character")
+             error.innerHTML=" password should be atleast 8 character with special charancter";
           }
       }else{
-          alert("enter valid email")
+        errorEmail.innerHTML="enter valid email";
       }
       
     });
